c046fa7d03732b491d7ff5f2476daac5efad1feb
OneBrunou <ms2689992@gmail.com>
2026-03-19 14:09:18 -0300

    chore: criando projeto1

---
+++ projeto1Asp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace projeto1Asp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
